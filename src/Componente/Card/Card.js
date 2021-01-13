63423ca2f030ec61088a215aa38c38451391d5c0
Paulo <pauloh16.pdr@gmail.com>
2021-01-13 16:01:03 -0300

    Criar componente Card para Dashboard

---
+++ src/Componente/Card/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+//  Importando estilo do componente.
+import estilos from "./Card.module.css";
+// Importando componentes da interface.
+import Icon from "../../Util/Icon.js";
+
+const Card = ({ cor, valor, texto, icon }) => {
+    return (
+        <div className={`${estilos.Card} ${estilos[cor]}`}>
+            <h2>{valor}</h2>
+            <p>{texto}</p>
+            <Icon nome={icon} />
+        </div>
+    );
+};
+
+export default Card;
--- src/Componente/Dashboard/Dashboard.js
+++ src/Componente/Dashboard/Dashboard.js
@@ -1,12 +1,21 @@
 import React from "react";
+// Importando estilo do componente.
+import estilos from "./Dashboard.module.css";
 // Importando componentes da interface.
 import ContainerSessao from "../ContainerSessao/ContainerSessao.js";
 import TituloSessao from "../TituloSessao/TituloSessao.js";
+import Card from "../Card/Card.js";
 
 const Dashboard = () => {
     return (
         <ContainerSessao>
             <TituloSessao titulo="Dashboard" subtitulo="Versão 1.0" />
+
+            <div className={estilos.cards}>
+                <Card cor="verde" valor="R$ 10" texto="Total de créditos" icon="bank" />
+                <Card cor="vermelho" valor="R$ 10" texto="Total de débitos" icon="credit-card" />
+                <Card cor="azul" valor="R$ 0" texto="Valor consolidado" icon="money" />
+            </div>
         </ContainerSessao>
     );
 };
