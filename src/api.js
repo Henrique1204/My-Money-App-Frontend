eab4eb326950904540d0b519c2121a9fa994bd21
Paulo <pauloh16.pdr@gmail.com>
2021-01-23 12:33:06 -0300

    Implementar actions no reducer de autenticacao

--- src/api.js
+++ src/api.js
@@ -1,21 +1,22 @@
-const URL_BASE = "http://localhost:3003/api";
+const URL_BASE_API = "http://localhost:3003/api";
+const URL_BASE_OAPI = "http://localhost:3003/oapi";
 
 export const GET_SUMMARY = () => ({
-    url: `${URL_BASE}/billingCycles/summary`,
+    url: `${URL_BASE_API}/billingCycles/summary`,
     options: {
         method: "GET"
     }
 });
 
 export const GET_LIST = () => ({
-    url: `${URL_BASE}/billingCycles`,
+    url: `${URL_BASE_API}/billingCycles`,
     options: {
         method: "GET"
     }
 });
 
 export const POST_CYCLE = (body) => ({
-    url: `${URL_BASE}/billingCycles`,
+    url: `${URL_BASE_API}/billingCycles`,
     options: {
         method: "POST",
         headers: {
@@ -26,7 +27,7 @@ export const POST_CYCLE = (body) => ({
 });
 
 export const PUT_CYCLE = (body, id) => ({
-    url: `${URL_BASE}/billingCycles/${id}`,
+    url: `${URL_BASE_API}/billingCycles/${id}`,
     options: {
         method: "PUT",
         headers: {
@@ -37,10 +38,41 @@ export const PUT_CYCLE = (body, id) => ({
 });
 
 export const DELETE_CYCLE = (id) => ({
-    url: `${URL_BASE}/billingCycles/${id}`,
+    url: `${URL_BASE_API}/billingCycles/${id}`,
     options: {
         method: "DELETE"
     }
 });
 
-// export const 
+export const LOGIN = (body) => ({
+    url: `${URL_BASE_OAPI}/login`,
+    options: {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+});
+
+export const SIGNUP = (body) => ({
+    url: `${URL_BASE_OAPI}/login`,
+    options: {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+});
+
+export const VALIDAR_TOKEN = (token) => ({
+    url: `${URL_BASE_OAPI}/validarToken`,
+    options: {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(token)
+    }
+});
--- src/store/auth.js
+++ src/store/auth.js
@@ -1,9 +1,16 @@
+// Importando utilitários do redux.
 import { createSlice } from "@reduxjs/toolkit";
+// Importando actions da store.
+import { mostrarFeedback } from "./ui.js";
+// Importando configurações da API.
+import { VALIDAR_TOKEN } from "../api.js";
+
+const storageKey = "token";
 
 const slice = createSlice({
     name: "auth",
     initialState: {
-        user: localStorage.getItem("token") || null,
+        user: localStorage.getItem(storageKey) || null,
         validToken: false
     },
     reducers: {
@@ -11,16 +18,65 @@ const slice = createSlice({
             if (action.payload) {
                 state.validToken = true;
             } else {
+                localStorage.removeItem(storageKey)
                 state.validToken = false;
                 state.user = null;
             }
         },
         userFetched(state, action) {
-            localStorage.setItem("token", action.payload);
+            localStorage.setItem(storageKey, action.payload);
             state.user = action.payload;
             state.validToken = true;
         }
     }
 });
+
+export const { tokenValidated, userFetched } = slice.actions;
+
+export const submit = ({ url, options }) => async (dispatch) => {
+    try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+
+        if (res.ok === false) {
+            const feedbacks = json.errors.map((error) => ({
+                msg: error,
+                status: "erro"
+            }));
+
+            dispatch(mostrarFeedback(feedbacks));
+        } else {
+            dispatch(userFetched(res));
+        }
+
+    } catch ({ message }) {
+        dispatch(mostrarFeedback([{
+            msg: message,
+            status: "erro"
+        }]))
+    }
+}
+
+export const logout = () => (dispatch) => {
+    dispatch(tokenValidated(false));
+}
+
+export const validarToken = (payload) => async (dispatch) => {
+    try {
+        if (payload) {
+            const { url, options } = VALIDAR_TOKEN(payload);
+            const res = await fetch(url, options);
+            const json = await res.json();
+
+            if (res.ok === false) throw new Error("Erro no fetch!");
+
+            dispatch(tokenValidated(json.valid));
+        } else {
+            dispatch(tokenValidated(false));
+        }
+    } catch (e) {
+        dispatch(tokenValidated(false));
+    }
+}
 
 export default slice.reducer;
